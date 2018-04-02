e9770ea3f695ae8a86bb2b482f4c1326c7e5ad3d
Chan Yi <huichanyi@gmail.com>
2018-04-02 00:37:19 -0700

    Added gitignore

---
+++ EscapingBuilding/Source/EscapingBuilding/EscapingBuildingGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "EscapingBuildingGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPINGBUILDING_API AEscapingBuildingGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
